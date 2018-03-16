fc7e20b6a95205d55d8fd6eb08e0876c95757e60
Jonny Cheetham <jcheetham@amplience.com>
2018-03-16 16:40:58 +0000

    MVP working

--- js/default.js
+++ js/default.js
@@ -1,6 +1,12 @@
 (($, _) => {
   $(() => {
     const url = "http://c1-qa.adis.ws/v1/content/cmslabs/content-item/0cc6e904-1922-41d6-8cc6-163c9a2d1e67?template=lateam&returnInlinedContent=true"
+    const template = `<div class="hero">
+                        <div class="hero__contents">
+                            <h1 class="display-4"></h1>
+                            <button type="button" class="btn btn-primary btn-lg">Shop now</button>
+                          </div>
+                        </div>`;                        
     $.ajax({url}).done(response => {
       // content.segment
       const data = response.segments;
@@ -22,8 +28,22 @@
       const type = _.maxBy(persona.segments.type, 'count');
       const genderType = `${type.value}-${gender.value}`;
       const content = _.chain(data).find({segment: genderType}).get('content').value();
-      if(content) {
-        
+      if(content && isHome) {
+        //hero--middle hero--super hero--large
+        //hero--long col-6
+        $('#default').hide();
+        _.forEach(content, item => {
+          const $el = $(template);
+          const imageUrl = `http://${item.image.defaultHost}/i/${item.image.endpoint}/${item.image.name}`;
+          if (item.super) {
+            $el.addClass('hero--middle hero--super hero--large col-12');
+          } else {
+            $el.addClass('hero--long col-6');
+          }
+          $el.find('h1').text(`${gender.value} ${type.value}`);
+          $el.find('.hero__contents').css({backgroundImage: `url(${imageUrl})`});
+          $('#personalised').append($el);
+        });
       }
     });
   });
